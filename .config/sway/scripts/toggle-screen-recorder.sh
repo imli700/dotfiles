dd9b28bdae0e79d7facadc3f123c86b46a9d4816
imli700 <imlijangba@gmail.com>
2024-03-18 14:52:06 +0530

    Add screen recorder changes
    I added scripts that toggle different wf-recorder presets and linked
    them to my sway config file

---
+++ .config/sway/scripts/toggle-screen-recorder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pid=$(pgrep wf-recorder)
+status=$?
+
+if [ $status != 0 ]; then
+	wf-recorder -f ~/Videos/screen-recordings/$(date +'recording-%Y_%m_%d-%Hh_%Mm_%Ss.mp4')
+else
+	pkill --signal SIGINT wf-recorder
+fi
