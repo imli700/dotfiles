d72b48254912e6cef6fd6aac2e84bfb6e7b28519
imli700 <imlijangba@gmail.com>
2026-06-22 13:31:32 +0530

    Configure cemu mode

--- .config/sway/cemu_check.sh
+++ .config/sway/cemu_check.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-USER_HOME="/home/imli700"
+USER_HOME="/home/your_username_here"
 STATE_FILE="$USER_HOME/.config/sway/.cemu_mode"
 MOUSE="Logitech G300s Optical Gaming Mouse"
 KEYBOARD="AT Translated Set 2 keyboard"
-PRESET="botw"
+GAME_PRESET="botw"
+DESKTOP_PRESET="desktop"
 
 if [ -f "$STATE_FILE" ]; then
-  /usr/bin/input-remapper-control --command start --device "$MOUSE" --preset "$PRESET"
-  /usr/bin/input-remapper-control --command start --device "$KEYBOARD" --preset "$PRESET"
+  /usr/bin/input-remapper-control --command start --device "$MOUSE" --preset "$GAME_PRESET"
+  /usr/bin/input-remapper-control --command start --device "$KEYBOARD" --preset "$GAME_PRESET"
 else
-  /usr/bin/input-remapper-control --command stop --device "$MOUSE"
+  /usr/bin/input-remapper-control --command start --device "$MOUSE" --preset "$DESKTOP_PRESET"
   /usr/bin/input-remapper-control --command stop --device "$KEYBOARD"
 fi
--- .config/sway/cemu_toggle.sh
+++ .config/sway/cemu_toggle.sh
@@ -2,19 +2,17 @@
 STATE_FILE="$HOME/.config/sway/.cemu_mode"
 MOUSE="Logitech G300s Optical Gaming Mouse"
 KEYBOARD="AT Translated Set 2 keyboard"
-PRESET="botw"
+GAME_PRESET="botw"
+DESKTOP_PRESET="desktop"
 
 if [ -f "$STATE_FILE" ]; then
-  # Turn OFF: Remove state file and stop remapper for both
   rm "$STATE_FILE"
-  input-remapper-control --command stop --device "$MOUSE"
   input-remapper-control --command stop --device "$KEYBOARD"
+  input-remapper-control --command start --device "$MOUSE" --preset "$DESKTOP_PRESET"
 else
-  # Turn ON: Create state file and start remapper for both
   touch "$STATE_FILE"
-  input-remapper-control --command start --device "$MOUSE" --preset "$PRESET"
-  input-remapper-control --command start --device "$KEYBOARD" --preset "$PRESET"
+  input-remapper-control --command start --device "$MOUSE" --preset "$GAME_PRESET"
+  input-remapper-control --command start --device "$KEYBOARD" --preset "$GAME_PRESET"
 fi
 
-# Send Signal 8 to Waybar to instantly update the UI indicator
 pkill -RTMIN+8 waybar
