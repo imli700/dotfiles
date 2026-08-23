312f36a8dbf8cdd60edfd06e189a8e186a01ed38
imli700 <imlijangba@gmail.com>
2026-08-23 15:08:23 +0530

    Change bitwarden config in qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -129,7 +129,9 @@ config.bind("<", "tab-move -")
 config.bind(">", "tab-move +")
 
 # Bitwarden autofill - trigger with ,p in normal mode
-config.bind(",p", "spawn --userscript qute-bitwarden")
+config.bind(",p", "spawn --userscript qute-rbw")
+config.bind(",pu", "spawn --userscript qute-rbw user")
+config.bind(",pp", "spawn --userscript qute-rbw pass")
 
 # ===================================================================
 # === Download bar ===
