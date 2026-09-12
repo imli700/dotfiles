bff7c8acde796b322d0bac0e32f31225ebe2406e
imli700 <imlijangba@gmail.com>
2026-09-12 17:58:36 +0530

    Qutebrowser catppuccin theme

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -50,9 +50,18 @@ config.load_autoconfig()
 #        )
 
 # ===================================================================
-# === Gruvbox theme (UI) ===
+# === Catppuccin theme (UI) ===
 # ===================================================================
-config.source("./themes/gruvbox.py")
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+
+    theme.setup(c, "mocha", True)
 
 
 # ===================================================================
